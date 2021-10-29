64f2f94de46b3a673c186c7cc73bfd035fcc2cef
Slail Billicko Jean Pierre <sbillicko@gmail.com>
2021-10-29 16:49:46 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def selectionSort(array):
+    """
+    A function that takes in an array of integers and returns a sorted version of that array using the Selection
+    Sort algorithm
+    """
+    pass
